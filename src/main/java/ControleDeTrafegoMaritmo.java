9e49845b504b2d3b2809ff74c125f456109839ec
Edmilson <scovinowork@gmail.com>
2021-11-18 14:59:35 -0300

    testes

--- src/main/java/ControleDeTrafegoMaritmo.java
+++ src/main/java/ControleDeTrafegoMaritmo.java
@@ -8,17 +8,17 @@ public class ControleDeTrafegoMaritmo {
     }
 
     public String recebePermissaoDoPorto(String mensagem){
-        return  "Recebemos o seu contato\n"+
+        return  "Recebemos o seu contato.\n"+
                 "O Porto Juvenau Carneiro, responde conforme a solicitação\n"+
                 ">>" +  PortoJuvenauCarneiro.getInstancia().recebePermissaoDeAncorar(mensagem);
     }
     public String recebeAvisoDeColisaoParaPorto(String mensagem){
-        return  "Recebemos o seu contato\n"+
+        return  "Recebemos o seu contato.\n"+
                 "O Porto Juvenau Carneiro, responde conforme a solicitação\n"+
                 ">>" +  PortoJuvenauCarneiro.getInstancia().recebeAvisoDeColisao(mensagem);
     }
     public String recebeAvisoDeFalhaMecanicaNoPorto(String mensagem){
-        return  "Recebemos o seu contato\n"+
+        return  "Recebemos o seu contato.\n"+
                 "O Porto Juvenau Carneiro, responde conforme a solicitação\n"+
                 ">>" +  PortoJuvenauCarneiro.getInstancia().recebeAvisoDeFalhaMecanica(mensagem);
     }
